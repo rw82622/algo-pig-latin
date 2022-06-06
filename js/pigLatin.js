95d472caeaabfb6c5e8fab5e6e41479a207d1638
Rexford Wiafe <w.rexford@yahoo.com>
2022-06-06 16:11:31 -0500

    solve pig latin in js

--- js/pigLatin.js
+++ js/pigLatin.js
@@ -1,2 +1,25 @@
-exports.translate = function(word) {
+exports.translate = function (word) {
+  let wordList = word.split(" ");
+  for (let index in wordList) {
+    temp = wordList[index];
+    if (temp[0].match(/[aeiou]/i)) {
+      temp += "ay";
+    } else {
+      let vowelIndex = temp.indexOf(temp.match(/[aeio]/i));
+      if (temp.match(/[!,.?;:]/)) {
+        temp =
+          temp.slice(vowelIndex, temp.length - 1) +
+          temp.slice(0, vowelIndex) +
+          "ay" +
+          temp.slice(temp.length - 1);
+      } else {
+        temp = temp.slice(vowelIndex) + temp.slice(0, vowelIndex) + "ay";
+      }
+      if (wordList[index][0].match(/[A-Z]/)) {
+        temp = temp[0].toUpperCase() + temp.slice(1).toLowerCase();
+      }
+    }
+    wordList[index] = temp;
+  }
+  return wordList.join(" ");
 };
